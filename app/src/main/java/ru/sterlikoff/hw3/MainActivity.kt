63e2ad2532f4cffc0fdb9beb172519cb52559032
sterlikov <717720@list.ru>
2020-03-16 17:20:54 +0300

    commit

--- app/src/main/java/ru/sterlikoff/hw3/MainActivity.kt
+++ app/src/main/java/ru/sterlikoff/hw3/MainActivity.kt
@@ -2,11 +2,51 @@ package ru.sterlikoff.hw3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val post = Post("My first post", "Danill Sterlikov", 170484646)
+
+        post.like()
+        post.like()
+
+        post.comment()
+        post.comment()
+        post.comment()
+
+        post.rePost()
+
+        val title = findViewById<TextView>(R.id.post_title)
+        val date = findViewById<TextView>(R.id.post_date)
+        val author = findViewById<TextView>(R.id.author)
+
+        val likeCount = findViewById<TextView>(R.id.like_count)
+        val commentCount = findViewById<TextView>(R.id.comment_count)
+        val shareCount = findViewById<TextView>(R.id.share_count)
+
+        val likeBtn = findViewById<ImageView>(R.id.btn_like)
+
+        likeBtn.setOnClickListener {
+
+            likeCount.setTextColor(resources.getColor(R.color.colorAccent))
+            likeBtn.setImageDrawable(resources.getDrawable(R.drawable.ic_thumb_up_accent_24dp))
+
+        }
+
+        title.text = post.title
+        date.text = post.getAgoString()
+        author.text = post.author
+        likeCount.text = post.likeCount.toString()
+        commentCount.text = post.commentCount.toString()
+        shareCount.text = post.rePostCount.toString()
+
     }
+
 }
---
+++ app/src/main/java/ru/sterlikoff/hw3/Post.kt
@@ -0,0 +1,75 @@
+package ru.sterlikoff.hw3
+
+open class Post(
+
+    var title: String,
+    var author: String,
+    var time: Int
+
+) {
+
+    var likeCount: Int = 0
+    var commentCount: Int = 0
+    var rePostCount: Int = 0
+
+    fun like() {
+        likeCount++
+    }
+
+    fun comment() {
+        commentCount++
+    }
+
+    fun rePost() {
+        rePostCount++
+    }
+
+    fun numericalWordForm(value: Long, form1: String, form2: String, form3: String): String {
+
+        if (value in 10..20) return form3
+
+        return when (value % 10) {
+
+            1L -> form1
+            in 2L..4L -> form2
+            else -> form3
+        }
+
+    }
+
+    fun timeAgoString(timeAgo: Long): String {
+
+        if (timeAgo < 60) return "менее минуты назад"
+
+        if (timeAgo < 60 * 60) {
+
+            val m: Long = timeAgo / 60
+            return "$m ${numericalWordForm(m, "минуту", "минуты", "минут")} назад"
+
+        }
+
+        if (timeAgo < 60 * 60 * 24) {
+
+            val h: Long = timeAgo / (60 * 60)
+            return "$h ${numericalWordForm(h, "час", "часа", "часов")} назад"
+
+        }
+
+        if (timeAgo <= 60 * 60 * 24 * 365) {
+
+            val d: Long = timeAgo / (60 * 60 * 24)
+            return "$d ${numericalWordForm(d, "день", "дня", "дней")} назад"
+
+        }
+
+        return "больше года назад"
+
+    }
+
+    fun getAgoString(): String {
+
+        return timeAgoString(System.currentTimeMillis() - time)
+
+    }
+
+}
