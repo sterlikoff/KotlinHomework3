09c6b36b7826806ec3cbf68de50969b1c3bdf3a3
sterlikov <717720@list.ru>
2020-04-10 18:10:39 +0300

    commit

--- app/src/main/java/ru/sterlikoff/hw3/MainActivity.kt
+++ app/src/main/java/ru/sterlikoff/hw3/MainActivity.kt
@@ -4,106 +4,53 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import io.ktor.client.HttpClient
+import io.ktor.client.features.json.GsonSerializer
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
+import kotlinx.coroutines.*
 import ru.sterlikoff.hw3.adapters.PostAdapter
-import ru.sterlikoff.hw3.interfaces.Item
 import ru.sterlikoff.hw3.models.Post
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
+
+    private fun fetch() = launch {
+
+        val list = withContext(Dispatchers.IO) {
+
+            val url =
+                "https://raw.githubusercontent.com/sterlikoff/PostGenerator/master/1.json"
+            val client = HttpClient {
+                install(JsonFeature) {
+                    acceptContentTypes = listOf(
+                        ContentType.Text.Plain,
+                        ContentType.Application.Json
+                    )
+                    serializer = GsonSerializer()
+
+                }
+            }
+
+            val res = client.get<List<Post>>(url)
+            client.close()
+
+            res
+
+        }
+
+        val listView = findViewById<RecyclerView>(R.id.itemList)
+        listView.adapter = PostAdapter(list, this@MainActivity)
+        listView.layoutManager = LinearLayoutManager(this@MainActivity)
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val list = mutableListOf<Item>()
-
-        list.add(
-            Post(
-                "Is Video and Event Post",
-                "Danill Sterlikov",
-                170484646,
-                15,
-                82,
-                3,
-                33.1546,
-                44.46847,
-                "https://www.youtube.com/watch?v=WhWc3b3KhnY"
-            )
-        )
-
-        list.add(
-            Post(
-                "Secondary post with very-very long title. Really very long title.",
-                "Ivan Ivanov",
-                170400000,
-                7,
-                81,
-                15
-            )
-        )
-
-        list.add(
-            Post(
-                "Is Event Post",
-                "Kolya",
-                170400999,
-                71,
-                810,
-                1,
-                33.1546,
-                44.46847
-            )
-        )
-
-        val sourcePost = Post(
-            "Is only video Post",
-            "Kolya",
-            170400999,
-            71,
-            810,
-            1,
-            null,
-            null,
-            "https://www.youtube.com/watch?v=WhWc3b3KhnY"
-        )
-
-        list.add(sourcePost)
-
-        list.add(
-            Post(
-                "Is sharing of previous",
-                "Marya Petrosyan",
-                170401000,
-                3,
-                1550,
-                0,
-                null,
-                null,
-                null,
-                sourcePost
-            )
-        )
-
-        list.add(
-            Post(
-                "Is Advertising",
-                "Google",
-                180400999,
-                0,
-                0,
-                0,
-                null,
-                null,
-                null,
-                null,
-                "https://google.com"
-            )
-        )
-
-        val listView = findViewById<RecyclerView>(R.id.itemList)
-        listView.adapter = PostAdapter(list, this)
-        listView.layoutManager = LinearLayoutManager(this)
-
+        fetch()
 
     }
 
